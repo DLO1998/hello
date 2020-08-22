40f12879be4b08e4a1a171b6e6706567db6f252e
Dylan Lowery <dylan.lowery2@huskers.unl.edu>
2020-08-22 01:18:21 +0000

    intial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Dylan Lowery
+ * Date: 2020/8/21
+ *
+ * A simple hello world program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Dylan Lowery\n");
+
+    return 0;
+
+}
